f2f656309b8ff3bbfae1f80dc6190d06342a0e8c
Peter 'Pita' Martischka <petermartischka@googlemail.com>
2012-06-09 22:18:27 +0100

    started to rewrite getParameter for the new api

--- tests/selenium/spec/getParameters.js
+++ tests/selenium/spec/getParameters.js
@@ -5,14 +5,42 @@ exports.name = "Does passing a URL parameter work?";
 exports.func= function(options, callback){
   var browser = options.browser;
   var assert = options.assert;
-  var location;
+  var baseURL = options.config.url + "/p/" + options.padID;
 
-  browser.chain
-  .session()
-  .setTimeout(20000)
+  browser
+  //load pad without parameter
+  .get(baseURL)
+  .waitForElement("css selector", "#editorcontainer iframe", 10000)
+  
+  //check if editbar and chat is visible if no parameters are passed
+  .isVisible("id", "editbar", function(err, visible){
+    assert.equal(visible, true, "Editbar is not visable in normal view");
+  })
+  .isVisible("id", "chaticon", function(err, visible){
+    assert.equal(visible, true, "Chat is not visable in normal view");
+  })
+
+  //load pad with showControls = false
+  .get(baseURL + "?showControls=false")
+  .waitForElement("css selector", "#editorcontainer iframe", 10000)
+
+  .isVisible("id", "editbar", function(err, visible){
+    assert.equal(visible, false, "Editbar is still visable with ?showControls=false");
+  })
+
+  //load pad with showChat = false
+  .get(baseURL + "?showChat=false")
+  .waitForElement("css selector", "#editorcontainer iframe", 10000)
+
+  .isVisible("id", "chaticon", function(err, visible){
+    assert.equal(visible, false, "Chat is still visable with ?showChat=false");
+  })
+
+  callback();
+
 
   // test hiding the toolbar
-  .open('/p/'+ options.padID)
+  /*.open('/p/'+ options.padID)
   .waitForElementPresent('id=innerdocbody')
   .assertVisible('id=editbar')
   .open('/p/'+ options.padID + '?showControls=false')
@@ -46,6 +74,6 @@ exports.func= function(options, callback){
   .assertVisible('id=sidediv')
   .open('/p/'+ options.padID + '?showLineNumbers=false')
   .waitForElementPresent('id=innerdocbody')
-//  .verifyAttribute('css=#sidediv', 'sidedivhidden') // this is broken.
+//  .verifyAttribute('css=#sidediv', 'sidedivhidden') // this is broken.*/
 
 }
